6167c3fd1d99cc3b1f923f413d97f77765641f27
Robert Sunbury <sunrobbie00@gmail.com>
2022-02-25 12:34:49 -0500

    Implemented basic data reading functionality

--- read_data.m
+++ read_data.m
@@ -1,4 +1,4 @@
-function [distance,pwm,target,deadpan] = read_data(device)
+function [distance,manual_pwm,target,deadpan] = read_data(device)
 %% Reads data sent back from Ball and Pipe system
 % Inputs:
 %  ~ device: serialport object controlling the real world system
@@ -12,21 +12,26 @@ function [distance,pwm,target,deadpan] = read_data(device)
 %  system
 %
 % Created by:  Kyle Naddeo 1/3/2022
-% Modified by: YOUR NAME and DATE
+% Modified by: Robbie Sunbury 2/5/2022
 
 %% Ask nicely for data
 % use the serialport() command options to write the correct letter to the
 % system (Hint: the letters are in the spec sheet)
+write(device, "S", "uint8");
 
 %% Read data
 % use the serialport() command options to read the response
+resultStr = read(device, 16, "unit8");
+resultStrSplit = split(resultStr, ",");
+
+resultStrSplit(1) = extractBetween(resultStrSplit(1), 2, 5);
 
 %% Translate
 % translate the response to 4 doubles using str2double() and
 % extractBetween() (Hint: the response is in the spec sheet)
-% distance   = 
-% manual_pwm = 
-% target     = 
-% deadpan    = 
+distance   = str2double(resultStrSplit(1));
+manual_pwm = str2double(resultStrSplit(2));
+target     = str2double(resultStrSplit(3));
+deadpan    = str2double(resultStrSplit(4));
 
 end
