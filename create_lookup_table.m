1af633c75f1c3e13ef37c7410a5785a4dbb829c1
Logan Greif <lgreif27@gmail.com>
2022-02-25 13:45:09 -0500

    Updare function with start end valies that are closer, need to fix these

--- create_lookup_table.m
+++ create_lookup_table.m
@@ -2,19 +2,23 @@ function [values] = create_lookup_table(device)
 %Create lookup table of values by sweeping the usable range of PWM values
 %   Detailed explanation goes here
 
-start_value = 123;
-end_value = 333;
+start_value = 1650;
+end_value = 1950;
 
-set_pwm(device, 1950); % Initial burst to pick up ball
-pause(0.1) % Wait 0.1 seconds
+set_pwm(device, 3000); % Initial burst to pick up ball
+pause(0.5);
 
 steps = start_value:5:end_value;
 sensor_readings = zeros(size(steps),1);
 
+% go to first step and stabilize before starting sweep
+set_pwm(device,start_value;
+pause(10);
+
 
 for i = steps
     set_pwm(device,i);
-    pause(0.1);
+    pause(0.5);
     sensor_readings[i] = read_data(device)(1);
 end
 
