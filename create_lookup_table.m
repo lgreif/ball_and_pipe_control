4732f7c85e7e663a0e9e74238bddc30973ef9c76
Logan Greif <lgreif27@gmail.com>
2022-04-01 11:31:14 -0400

    changes from last lab period

--- create_lookup_table.m
+++ create_lookup_table.m
@@ -2,25 +2,29 @@ function [values] = create_lookup_table(device)
 %Create lookup table of values by sweeping the usable range of PWM values
 %   Detailed explanation goes here
 
-start_value = 1650;
-end_value = 1950;
+start_value = 2800;
+end_value = 2500;
 
 set_pwm(device, 3000); % Initial burst to pick up ball
 pause(0.5);
 
-steps = start_value:5:end_value;
-sensor_readings = zeros(size(steps),1);
+step_size = 20;
+
+steps = start_value:step_size*-1:end_value;
+sensor_readings = zeros((start_value - end_value)/step_size + 1,1);
 
 % go to first step and stabilize before starting sweep
-set_pwm(device,start_value;
-pause(10);
+set_pwm(device,3000);
+pause(4);
 
 
 for i = steps
     set_pwm(device,i);
-    pause(0.5);
-    sensor_readings[i] = read_data(device)(1);
+    pause(4);
+    data = read_data(device);
+    sensor_readings(i) = data(1);
 end
-
+steps
+sensor_readings
 values = [steps;sensor_readings]
 end
